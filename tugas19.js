cc2e5529d639c8c6430b143e836c9f67d39613b4
wildan <wildan.syarif1021@gmail.com>
2020-03-17 13:26:12 +0700

    tugas niomic

---
+++ tugas19.js
@@ -0,0 +1,17 @@
+function tugas19(){
+    let x = "Semangat...";
+    console.log(x.repeat(5));
+    let y = "*****";
+    console.log(y.repeat(1));
+    console.log(y.repeat(6));
+    console.log(y.repeat(11));
+    console.log(y.repeat(16));
+    console.log(y.repeat(21));
+    console.log(y.repeat(21));
+    console.log(y.repeat(16));
+    console.log(y.repeat(11));
+    console.log(y.repeat(6));
+    console.log(y.repeat(1));
+};
+
+tugas19();
